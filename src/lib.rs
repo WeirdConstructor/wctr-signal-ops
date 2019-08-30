bc5ad17c1f0956d6e06536c6eec6d67fcabc8c74
Weird Constructor <weirdconstructor@gmail.com>
2019-08-30 21:53:03 +0200

    more public exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,18 @@ pub mod sample_row;
 pub mod signals;
 pub mod ops;
 
+pub use signals::{
+    OpIn,
+    Op,
+    OpPort,
+    OpIOSpec,
+    OpInfo,
+    Simulator,
+    SimulatorUIEvent,
+    SimulatorUIInput,
+    SimulatorCommunicator,
+    SimulatorCommunicatorEndpoint};
+
 //#[cfg(test)]
 //mod tests {
 //    #[test]
